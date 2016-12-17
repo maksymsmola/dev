f9e4e59af5693bbbc3aabeb25a49591fed56ef18
maksymsmola <maksym.smola@gmail.com>
2016-12-17 16:35:38 +0200

    Added basic solution template

---
+++ MoneyKeeper.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MoneyKeeper.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public JsonResult SignIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonResult SignUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
