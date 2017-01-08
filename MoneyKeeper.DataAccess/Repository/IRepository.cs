3151ac0d3ef2d326463c459848244f940431e776
maksymsmola <maksym.smola@gmail.com>
2017-01-08 19:49:46 +0200

    Added Repository

---
+++ MoneyKeeper.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿// todo: encapsulate query logic using Specification pattern
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using MoneyKeeper.Core.Entities;
+
+namespace MoneyKeeper.DataAccess.Repository
+{
+    public interface IRepository
+    {
+        T FindById<T>(long id) where T : BaseEntity;
+
+        T FindByCriteria<T>(Expression<Func<T, bool>> criteria) where T : BaseEntity;
+
+        void Add<T>(T entity) where T : BaseEntity;
+
+        List<T> GetByCriteria<T>(Expression<Func<T, bool>> criteria) where T : BaseEntity;
+
+        void SaveChanges();
+    }
+}
---
+++ MoneyKeeper.DataAccess/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using MoneyKeeper.Core.Entities;
+
+namespace MoneyKeeper.DataAccess.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public T FindById<T>(long id) where T : BaseEntity
+        {
+            return this.context.Set<T>().Find(id);
+        }
+
+        public T FindByCriteria<T>(Expression<Func<T, bool>> criteria) where T : BaseEntity
+        {
+            return this.context.Set<T>().FirstOrDefault(criteria);
+        }
+
+        public void Add<T>(T entity) where T : BaseEntity
+        {
+            this.context.Set<T>().Add(entity);
+        }
+
+        public List<T> GetByCriteria<T>(Expression<Func<T, bool>> criteria) where T : BaseEntity
+        {
+            return this.context.Set<T>().Where(criteria).ToList();
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
