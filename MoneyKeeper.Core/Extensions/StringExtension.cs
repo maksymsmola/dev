6363d168bf1a9c12050c15499eb043329f907dc9
maksymsmola <maksym.smola@gmail.com>
2017-01-08 19:50:02 +0200

    Added String extensions

---
+++ MoneyKeeper.Core/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MoneyKeeper.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Encode(this string @string)
+        {
+            using (SHA256 hash = SHA256.Create())
+            {
+                IEnumerable<string> hashedBytes =
+                    hash.ComputeHash(Encoding.UTF8.GetBytes(@string))
+                    .Select(@byte => @byte.ToString("x2"));
+
+                return string.Concat(hashedBytes);
+            }
+        }
+    }
+}
