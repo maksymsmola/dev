593b4c56cb1ad0e734cff0638795bc7a5e378cff
maksymsmola <maksym.smola@gmail.com>
2017-05-10 18:26:53 +0300

    MIGRATION: added StatefulEntity

---
+++ MoneyKeeper.Core/Entities/EntityState.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyKeeper.Core.Entities
+{
+    public enum EntityState
+    {
+        None = 0,
+        Untouched = 1,
+        Added = 2,
+        Updated = 3,
+        Deleted = 4
+    }
+}
